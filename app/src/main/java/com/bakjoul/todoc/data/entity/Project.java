2210f01e56dc326bcbc6fda91b9dafdaf36dd401
bakjoul (L5P) <joul_77@hotmail.com>
2022-07-05 18:14:13 +0200

    Added room dependencies, created Project entity

---
+++ app/src/main/java/com/bakjoul/todoc/data/entity/Project.java
@@ -0,0 +1,63 @@
+package com.bakjoul.todoc.data.entity;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Objects;
+
+@Entity(tableName = "project")
+public class Project {
+
+    @PrimaryKey(autoGenerate = true)
+    private final long id;
+
+    @NonNull
+    private final String name;
+
+    @ColorInt
+    private final int color;
+
+    public Project(long id, @NonNull String name, int color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Project project = (Project) o;
+        return id == project.id && color == project.color && name.equals(project.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color=" + color +
+                '}';
+    }
+}
