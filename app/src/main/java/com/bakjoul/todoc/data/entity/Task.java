be19304c8c541a3ec59ef92b2845bfae77555e63
bakjoul (L5P) <joul_77@hotmail.com>
2022-07-05 18:40:26 +0200

    Created Task entity

---
+++ app/src/main/java/com/bakjoul/todoc/data/entity/Task.java
@@ -0,0 +1,92 @@
+package com.bakjoul.todoc.data.entity;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+@Entity(tableName = "task")
+public class Task {
+
+    @PrimaryKey(autoGenerate = true)
+    private long id;
+
+    @ColumnInfo(index = true)
+    private long projectId;
+
+    @NonNull
+    private String taskDescription;
+
+    public Task(long id, long projectId, @NonNull String taskDescription) {
+        this.id = id;
+        this.projectId = projectId;
+        this.taskDescription = taskDescription;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    @NonNull
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && projectId == task.projectId && taskDescription.equals(task.taskDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, projectId, taskDescription);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", projectId=" + projectId +
+                ", taskDescription='" + taskDescription + '\'' +
+                '}';
+    }
+
+    public static class TaskAZComparator implements Comparator<Task> {
+        @Override
+        public int compare(@NonNull Task left, @NonNull Task right) {
+            return left.taskDescription.compareTo(right.taskDescription);
+        }
+    }
+
+    public static class TaskZAComparator implements Comparator<Task> {
+        @Override
+        public int compare(@NonNull Task left, @NonNull Task right) {
+            return right.taskDescription.compareTo(left.taskDescription);
+        }
+    }
+
+    public static class TaskRecentComparator implements Comparator<Task> {
+        @Override
+        public int compare(@NonNull Task left, @NonNull Task right) {
+            return (int) (right.getId() - left.getId());
+        }
+    }
+
+    public static class TaskOldComparator implements Comparator<Task> {
+        @Override
+        public int compare(@NonNull Task left, @NonNull Task right) {
+            return (int) (left.getId() - right.getId());
+        }
+    }
+}
