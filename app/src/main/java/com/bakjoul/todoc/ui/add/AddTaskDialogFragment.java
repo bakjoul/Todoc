e36e2e37b6a151e77500199be9bb0bd334d89f50
bakjoul <bakjoul@gmail.com>
2022-07-15 11:41:05 +0200

    Updated AddTaskDialogFragment (@AndroidEntryPoint, view model)

--- app/src/main/java/com/bakjoul/todoc/ui/add/AddTaskDialogFragment.java
+++ app/src/main/java/com/bakjoul/todoc/ui/add/AddTaskDialogFragment.java
@@ -8,10 +8,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.bakjoul.todoc.R;
 import com.bakjoul.todoc.databinding.AddTaskDialogBinding;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class AddTaskDialogFragment extends DialogFragment {
 
     @NonNull
@@ -19,12 +23,12 @@ public class AddTaskDialogFragment extends DialogFragment {
         return new AddTaskDialogFragment();
     }
 
-    private AddTaskViewModel viewModel;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         AddTaskDialogBinding b = AddTaskDialogBinding.inflate(inflater, container, false);
+
+        AddTaskViewModel viewModel = new ViewModelProvider(this).get(AddTaskViewModel.class);
 
         final AddTaskProjectSpinnerAdapter adapter = new AddTaskProjectSpinnerAdapter(requireContext(), R.layout.add_task_project_spinner_item);
         b.addTaskProjectSpinnerActv.setAdapter(adapter);
