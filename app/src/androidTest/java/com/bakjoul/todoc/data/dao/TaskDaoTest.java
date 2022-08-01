8e70133f9d8f0dc323c4ce674d5fad983f940a1b
bakjoul (L5P) <joul_77@hotmail.com>
2022-08-01 17:35:59 +0200

    Added some tests in TaskDaoTest

--- app/src/androidTest/java/com/bakjoul/todoc/data/dao/TaskDaoTest.java
+++ app/src/androidTest/java/com/bakjoul/todoc/data/dao/TaskDaoTest.java
@@ -1,6 +1,10 @@
 package com.bakjoul.todoc.data.dao;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.room.Room;
@@ -8,12 +12,23 @@ import androidx.test.core.app.ApplicationProvider;
 
 import com.bakjoul.todoc.data.AppDatabase;
 import com.bakjoul.todoc.data.entity.Project;
+import com.bakjoul.todoc.data.entity.Task;
+import com.bakjoul.todoc.utils.LiveDataTestUtil;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class TaskDaoTest {
+
+    private static final int EXPECTED_PROJECT_ID = 1;
+    private static final String EXPECTED_TASK_DESCRIPTION = "taskDescription";
 
     @Rule
     public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
@@ -39,5 +54,31 @@ public class TaskDaoTest {
         appDatabase.close();
     }
 
+    @Test
+    public void insert_task() {
+        // Given
+        Task task = new Task(EXPECTED_PROJECT_ID, EXPECTED_TASK_DESCRIPTION);
 
+        // When
+        taskDao.insert(task);
+        List<Task> results = LiveDataTestUtil.getValueForTesting(taskDao.getAllTasks());
+
+        // Then
+        assertEquals(Collections.singletonList(new Task(1, EXPECTED_PROJECT_ID, EXPECTED_TASK_DESCRIPTION)), results);
+    }
+
+    @Test
+    public void delete_task() {
+        // Given
+        long taskId = 1;
+        Task task = new Task(taskId, EXPECTED_PROJECT_ID, EXPECTED_TASK_DESCRIPTION);
+        taskDao.insert(task);
+
+        // When
+        taskDao.delete(taskId);
+        List<Task> results = LiveDataTestUtil.getValueForTesting(taskDao.getAllTasks());
+
+        // Then
+        assertTrue(results.isEmpty());
+    }
 }
